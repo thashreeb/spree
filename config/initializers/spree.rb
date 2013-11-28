c83eb5a30f670063119e954d5b45609509cf4385
Thashreeb <thashreeb@gmail.com>
2013-11-28 11:53:21 +0530

    t commit

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,14 +1,9 @@
-# Configure Spree Preferences
-#
-# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
-#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
-#
-# In order to initialize a setting do:
-# config.setting_name = 'new value'
 Spree.config do |config|
-  # Example:
-  # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
+  config.use_s3 = true
+  config.s3_bucket = 'qapfybucket'
+  config.s3_access_key = "AKIAJPQH7NG7IBNFANCQ"
+  config.s3_secret = "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"
+ 
 end
 
 Spree.user_class = "Spree::LegacyUser"
